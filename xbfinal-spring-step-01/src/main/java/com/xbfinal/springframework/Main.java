f26672be1e2fa3051f4a1de4b13e42fbd39ab711
xbfinal <358863605@qq.com>
2023-10-07 20:18:03 +0800

    实现一个简单的Bean容器

---
+++ xbfinal-spring-step-01/src/main/java/com/xbfinal/springframework/Main.java
@@ -0,0 +1,13 @@
+package com.xbfinal.springframework;
+
+/**
+ * @version 1.0
+ * @Author 笑霸final
+ * @Date 2023/10/7 19:54
+ * @注释
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ xbfinal-spring-step-01/src/test/java/com/xbfinal/springframework/SpringTest.java
@@ -0,0 +1,25 @@
+package com.xbfinal.springframework;
+
+import org.junit.Test;
+
+/**
+ * @version 1.0
+ * @Author 笑霸final
+ * @Date 2023/10/7 20:12
+ * @注释
+ */
+public class SpringTest {
+    @Test
+    public void test_BeanFactory(){
+        BeanFactory beanFactory = new BeanFactory();
+
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+
+        beanFactory.registerBeanDefinition("userName",beanDefinition);
+
+        //获取bean
+        UserService userName = (UserService)beanFactory.getBeanDefinitionMap("userName");
+        userName.sayOk();
+
+    }
+}
